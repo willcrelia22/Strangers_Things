76188311cb9b6ceb0880a918da05a80d15176084
Carlos <chnava@me.com>
2022-07-06 20:32:27 -0500

    second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const app = () => {
-    return(<h1>APP COMPONENT</h1>)
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Strangers Things</h1>
 
+      <form>
+        <label>User Name</label>
+        <input id="username" defaultValue="Username Here"></input>
+        <label>Password</label>
+        <input id="password" defaultValue="Password Here"></input>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default app
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { App } from "./components";
 
-import { app } from './components'
-
-const root = ReactDOM.createRoot(document.getElementById('app'))
+const root = ReactDOM.createRoot(document.getElementById("App"));
 root.render(
-<BrowserRouter>
-<app />
-</BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
